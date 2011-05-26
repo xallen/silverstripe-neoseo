b0ab2b8ff46984df648adbc9096ab0da3320c668
Chris Allen <chris@llen.co.nz>
2011-05-26 15:44:10 +1200

    Introduced hackaround to force SiteConfig ID in YahooAnalyticsVariable. This does not seem to be a problem limited to me; I see UncleCheese addresses it here with a workaround in DOM: http://silverstripe.org/dataobjectmanager-module-forum/show/12048?start=0 I, however, did not want to force DOM as a requirement, even though it rocks.

--- code/YahooAnalyticsVariable.php
+++ code/YahooAnalyticsVariable.php
@@ -7,11 +7,23 @@
 			'Name' => 'Enum("DocumentName, DocumentGroup, MemberId, CmpQuery, CookieDomain, Domains, FlashUrl")',
 			'Value' => 'Varchar(128)'
 		);
+		
+		static $has_one = array(
+			'SiteConfig' => 'SiteConfig'
+		);
 	
 		static $summary_fields = array(
 			'Name',
 			'Value'
 		);
+		
+		/* HACK/TODO: Force SiteConfigID to current SiteConfig ID. I couldn't work
+		 * out why the relationship wasn't working to begin with.
+		 */
+		function onBeforeWrite() {
+			$this->SiteConfigID = SiteConfig::current_site_config()->ID;
+			parent::onBeforeWrite();
+		}
 		
 		/* NOTE: http://help.yahoo.com/l/us/yahoo/smallbusiness/store/analytics/advanced/advanced-02.html */
 	
