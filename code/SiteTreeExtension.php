565ab6a3c3279f25c228d7105de917d85b56c387
Chris Allen <chris@llen.co.nz>
2011-05-26 16:42:04 +1200

    Most of MetaManager's keyword suggestion logic has been crudely implemented.

--- code/SiteTreeExtension.php
+++ code/SiteTreeExtension.php
@@ -104,11 +104,194 @@
 		/* New/modified fields for the CMS. */
 		public function updateCMSFields($fields) {
 			
+			$suggested_keywords = $this->calculateKeywords($this->owner->Content, 4, 15, self::$exclude_words);
+			
+			/* Fill the suggestion field. This sets up the field so we can utilize Prototype later. */
+			$suggested_keywords_literal = "<p id=\"MetaKeywordsSuggestion\"><strong>Suggested keywords:</strong> <span id=\"NeoSEO_Keywords\">{$suggested_keywords}</span>.<br/><a id=\"NeoSEO_KeywordAppend\" href=\"#\">Append these keywords</a> | <a id=\"NeoSEO_KeywordReplace\" href=\"#\">Replace existing keywords with these keywords</a></p>";
+			
 			$fields->addFieldToTab('Root.Content.Metadata', new CheckboxField('MetaKeywordsAppend', 'Append global keywords?'), 'MetaDescription');
+			$fields->addFieldToTab('Root.Content.Metadata', new LiteralField('MetaKeywordsSuggestion', $suggested_keywords_literal), 'MetaKeywordsAppend');
 			$fields->addFieldToTab('Root.Content.Metadata', new DropdownField('MetaDescriptionAppend', 'Append global description?',  array('Beginning' => 'Yes, to the beginning', 'End' => 'Yes, to the end', 'No' => 'No')), 'ExtraMeta');
 			$fields->addFieldToTab('Root.Content.Metadata', new CheckboxField('ExtraMetaAppend', 'Append global custom meta tags?'));
 			
+			Requirements::javascript('neoseo/javascript/neoseo-sitetree-metadata.js');
+			
 			return $fields;
+		}
+		
+		/**
+		 * Extract Keywords
+		 * Returns a lowercase string with keywords ordered by occurance in content seperated with comma's
+		 * @var $string
+		 * @var $min_word_char
+		 * @var $keyword_amount
+		 * @var $exclude_words
+		 */
+		private function calculateKeywords($string = '', $min_word_char = 4, $keyword_amount = 15,  $exclude_words = '' ) {
+			
+			$exclude_words = explode(", ", $exclude_words);
+			//add space before br tags so words aren't concatenated when tags are stripped
+			$string = preg_replace('/\<br(\s*)?\/?\>/i', " <br />", $string); 
+			// get rid off the htmltags
+			$string = html_entity_decode(strip_tags($string), ENT_NOQUOTES , 'UTF-8');
+			
+			// count all words with str_word_count_utf8
+			$initial_words_array  = self::str_word_count_utf8($string, 1);
+			$total_words = sizeof($initial_words_array);
+			
+			$new_string = $string;
+			
+			//convert to lower case
+			$new_string = mb_convert_case($new_string, MB_CASE_LOWER, "UTF-8");
+			
+			// strip excluded words
+			foreach($exclude_words as $filter_word)	{
+				$new_string = preg_replace("/\b".$filter_word."\b/i", "", $new_string); 
+			}
+			
+			// calculate words again without the excluded words using str_word_count_utf8
+			$words_array = self::str_word_count_utf8($new_string, 1);
+			$words_array = array_filter($words_array, create_function('$var', 'return (strlen($var) >= '.$min_word_char.');'));
+			
+			$popularity = array();
+			$unique_words_array = array_unique($words_array);
+			
+			// create density array
+			foreach($unique_words_array as  $key => $word)	{
+				preg_match_all('/\b'.$word.'\b/i', $string, $out);
+				$count = count($out[0]);	
+				$popularity[$key]['count'] = $count;
+				$popularity[$key]['word'] = $word;
+				
+			}
+			
+			usort($popularity, array($this,'cmp'));
+			
+			// sort array form higher to lower
+			krsort($popularity);
+			
+			// create keyword array with only words
+			$keywords = array();
+			foreach($popularity as $value){
+							$keywords[] = $value['word']; 
+						}
+						
+			// glue keywords to string seperated by comma, maximum 15 words
+			$keystring =  implode(', ', array_slice($keywords, 0, $keyword_amount));
+			
+			// return the keywords
+			return $keystring;
+		}
+		
+		/**
+		 * @var int 
+		 */
+		protected static $keyword_amount = 15;
+		
+		/**
+		 * @var int
+		 */
+		protected static $min_word_char = 3;
+
+		/**
+		 * @var string
+		 */
+		protected static $exclude_words = '';
+		
+		/**
+		 * @var bool
+		 */
+		protected static $checkbox_state = 0;
+		
+		/**
+		 * @var string
+		 */
+		protected static $checkbox_tab = '';
+		
+		/**
+		 * @var string
+		 */
+		protected static $checkbox_pos = '';
+		
+		/**
+		 * @var bool
+		 */
+		protected static $show_checkbox = 0;
+		
+		/**
+		 * @var bool
+		 */
+		protected static $show_meta_messages = 0;
+		
+		/** 
+		 * @var bool
+		 */
+		protected static $update_meta_title = 0;
+		
+		/** 
+		 * @var bool
+		 */
+		protected static $update_meta_desc = 0;
+		
+		/** 
+		 * @var int
+		 */
+		protected static $meta_desc_length = 0;
+		
+		/** 
+		 * @var bool
+		 */
+		protected static $update_meta_keys = 0;
+		
+		/**
+		 * @var bool
+		 */
+		protected static $hide_extra_meta = 0;
+		
+		/**
+		 * @var bool
+		 */
+		protected static $disable_update_popup = 0;
+		
+		/**
+		 * @var bool
+		 */
+		protected static $update_url = 0;
+		
+		/**
+		 * Sort array by count value
+		 */
+		private static function cmp($a, $b) {
+			return ($a['count'] > $b['count']) ? +1 : -1;
+		}
+
+		/** Word count for UTF8
+		/* Found in: http://www.php.net/%20str_word_count#85592
+		/* The original mask contained the apostrophe, not good for Meta keywords:
+		/* "/\p{L}[\p{L}\p{Mn}\p{Pd}'\x{2019}..."
+		*/
+		private static function str_word_count_utf8($string, $format = 0) {
+			switch ($format) {
+			case 1:
+				preg_match_all("/\p{L}[\p{L}\p{Mn}\p{Pd}]*/u", $string, $matches);
+				return $matches[0];
+			case 2:
+				preg_match_all("/\p{L}[\p{L}\p{Mn}\p{Pd}]*/u", $string, $matches, PREG_OFFSET_CAPTURE);
+				$result = array();
+				foreach ($matches[0] as $match) {
+					$result[$match[1]] = $match[0];
+				}
+				return $result;
+			}
+			return preg_match_all("/\p{L}[\p{L}\p{Mn}\p{Pd}]*/u", $string, $matches);
+		}	
+
+		/**
+		 * Set the words to exlude seperate with comma
+		 * @param string $exclude_words 
+		 */
+		public function set_exclude_words($var) {
+			if ($var) self::$exclude_words = $var;
 		}
 		
 	}
