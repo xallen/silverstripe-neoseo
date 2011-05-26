79fc4a33a7cd578ff0a87f76689b75b7731fda3b
Chris Allen <chris@llen.co.nz>
2011-05-26 16:39:35 +1200

    Beginning (partial) integration of MetaManager. JavaScript and CSS is complete. Excluded words have had Swedish removed because they are now unmaintainable by me personally.

--- _config.php
+++ _config.php
@@ -5,5 +5,7 @@
 	DataObject::add_extension('SiteTree', 'SiteTreeExtension');
 	
 	LeftAndMain::require_css('neoseo/css/neoseo-leftandmain.css');
+	
+	SiteTreeExtension::set_exclude_words('about, again, also, been, before, cause, come, could, default, does, each, edit, even, from, give, have, here, just, like, made, many, most, much, must, only, other, said, same, should, since, some, such, tell, than, that, their, them, then, there, these, they, thing, this, through, very, want, welcome, well, were, what, when, where, which, while, will, with, within, would, your');
 
 ?>
---
+++ javascript/neoseo-sitetree-metadata.js
@@ -0,0 +1,17 @@
+Behaviour.register({
+	/* Handle appending the suggested keywords to the MetaKeywords TextField. */
+	'a#NeoSEO_KeywordAppend': {
+		onclick: function() {
+			$('Form_EditForm_MetaKeywords').value += (", " + $('NeoSEO_Keywords').innerHTML);
+		}
+	},
+	/* Handle replacing the keywords in MetaKeywords TextField with the suggested keywords. */
+	'a#NeoSEO_KeywordReplace': {
+		onclick: function() {
+			/* Warn the user and give them a way out. */
+			if(confirm("This will replace your existing keywords. Proceed?")) {
+				$('Form_EditForm_MetaKeywords').value = $('NeoSEO_Keywords').innerHTML;
+			}
+		}
+	}
+});
