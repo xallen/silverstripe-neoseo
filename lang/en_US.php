cd2bb77f2d9fb5df4f9f60ee6e88009b0db6d9f4
Chris Allen <chris@llen.co.nz>
2011-05-31 18:21:09 +1200

    Started addition of languages with a placeholder/template.

---
+++ lang/en_US.php
@@ -0,0 +1,17 @@
+<?php
+
+global $lang;
+
+/* Left as a sample. */
+$lang['en_US']['AssetAdmin']['CHOOSEFILE'] = 'Choose file: ';
+$lang['en_US']['AssetAdmin']['DELETEDX'] = 'Deleted %s files.%s';
+$lang['en_US']['AssetAdmin']['FILESREADY'] = 'Files ready to upload:';
+$lang['en_US']['AssetAdmin']['FOLDERDELETED'] = 'folder deleted.';
+$lang['en_US']['AssetAdmin']['FOLDERSDELETED'] = 'folders deleted.';
+$lang['en_US']['AssetAdmin']['MENUTITLE'] = array(
+	'Files & Images',
+	100,
+	'Menu title'
+);*/
+
+?>
