ffac1a8e7dd2033643a137ee61f6b42253f93e86
Chris Allen <chris@llen.co.nz>
2011-05-31 12:40:06 +1200

    Custom description can now be forced on (beginning or end) and off - on a SiteTree-wide basis.

--- code/LeftAndMainExtension.php
+++ code/LeftAndMainExtension.php
@@ -5,6 +5,7 @@
 		/* List of functions allowed to handle requests. */
 		static $allowed_actions = array('force_append_setting');
 
+		/* TODO: Add Subsite support - further filter consideration. */
 		public function force_append_setting() {
 		
 			/* We need these paramaters from the POST data. */
@@ -14,11 +15,14 @@
 			/* Handle the actual DB manipulation here. */
 			switch($name) {
 				case 'MetaKeywords':
-					DB::query('UPDATE `SiteTree` SET `MetaKeywordsAppend` = '.(($value == 'Yes') ? 1 : 0).' WHERE `ID` = '.SiteConfig::current_site_config()->ID);
+					DB::query('UPDATE `SiteTree` SET `MetaKeywordsAppend` = '.(($value == 'Yes') ? 1 : 0));
 					return 'statusMessage("Global metadata keywords have been '.(($value == 'Yes') ? 'enabled' : 'disabled').' for all pages.", "good");';
 				case 'MetaDescription':
+					if($value != 'Beginning' and $value != 'End') $value = 'No'; /* Make sure the data is safe. If it isn't Beginning or End it is set to No. */
+					DB::query('UPDATE `SiteTree` SET `MetaDescriptionAppend` = \''.$value.'\'');
+					return 'statusMessage("Global metadata description has been '.(($value == 'No') ? 'disabled' : 'enabled').' on all pages.", "good");';
 				case 'ExtraMeta':
-					DB::query('UPDATE `SiteTree` SET `ExtraMetaAppend` = '.(($value == 'Yes') ? 1 : 0).' WHERE `ID` = '.SiteConfig::current_site_config()->ID);
+					DB::query('UPDATE `SiteTree` SET `ExtraMetaAppend` = '.(($value == 'Yes') ? 1 : 0));
 					return 'statusMessage("Global custom metadata tags have been '.(($value == 'Yes') ? 'enabled' : 'disabled').' for all pages.", "good");';
 			}
 			
