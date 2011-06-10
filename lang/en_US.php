71c96204998e566b5b7a220da30268d371c5bd36
Chris Allen <chris@llen.co.nz>
2011-06-10 16:57:52 +1200

    Started building language file.

--- lang/en_US.php
+++ lang/en_US.php
@@ -2,12 +2,10 @@
 
 global $lang;
 
+$lang['en_US']['SiteTree']['BUTTONHYPE'] = 'Hype';
+
 /* Left as a sample.
 $lang['en_US']['AssetAdmin']['CHOOSEFILE'] = 'Choose file: ';
-$lang['en_US']['AssetAdmin']['DELETEDX'] = 'Deleted %s files.%s';
-$lang['en_US']['AssetAdmin']['FILESREADY'] = 'Files ready to upload:';
-$lang['en_US']['AssetAdmin']['FOLDERDELETED'] = 'folder deleted.';
-$lang['en_US']['AssetAdmin']['FOLDERSDELETED'] = 'folders deleted.';
 $lang['en_US']['AssetAdmin']['MENUTITLE'] = array(
 	'Files & Images',
 	100,
